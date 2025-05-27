28cd827d04f281bca1f260a5d9d01a6ab8b72c09
mauroac26 <mauroac26@gmail.com>
2025-05-27 16:20:51 -0300

    se creo crud Ocupacion

--- administracion/urls.py
+++ administracion/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index_administracion, name='index_administracion'),
+
     path('listadoEspecialidad/', views.ListEspecialidad.as_view(), name='listadoEspecialidad'),
     path('registroEspecialidad/', views.RegistroEspecialidadView.as_view(), name='registroEspecialidad'),
     path('editarEspecialidad/<int:pk>', views.ActualizarEspecialidadView.as_view(), name='editarEspecialidad'),
     path('eliminarEspecialidad/<int:pk>/', views.EliminarEspecialidadView.as_view(), name='eliminarEspecialidad'),
+
+    path('listadoOcupacion/', views.ListOcupacion.as_view(), name='listadoOcupacion'),
+    path('registroOcupacion/', views.RegistroOcupacionView.as_view(), name='registroOcupacion'),
+    path('editarOcupacion/<int:pk>', views.ActualizarOcupacionView.as_view(), name='editarOcupacion'),
+    path('eliminarOcupacion/<int:pk>/', views.EliminarOcupacionView.as_view(), name='eliminarOcupacion'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -13,14 +13,14 @@ def index_administracion(request):
 #VISTA ESPECIALIDAD BASADAS EN CLASE
 class ListEspecialidad(ListView):
     model = Especialidad
-    template_name = 'administracion/profesional/listadoEspecialidades.html'  
+    template_name = 'administracion/profesional/especialidad/listadoEspecialidades.html'  
     context_object_name = 'especialidades'  
 
     
 class RegistroEspecialidadView(CreateView):
     model = Especialidad
     form_class = EspecialidadForm
-    template_name = 'administracion/profesional/registroEspecialidad.html'
+    template_name = 'administracion/profesional/especialidad/registroEspecialidad.html'
     success_url = reverse_lazy('listadoEspecialidad')
 
     def form_valid(self, form):
@@ -31,7 +31,7 @@ class RegistroEspecialidadView(CreateView):
 class ActualizarEspecialidadView(UpdateView):
     model = Especialidad
     form_class = EspecialidadForm
-    template_name = 'administracion/profesional/editarEspecialidad.html'
+    template_name = 'administracion/profesional/especialidad/editarEspecialidad.html'
     success_url = reverse_lazy('listadoEspecialidad')
 
     def form_valid(self, form):
@@ -41,7 +41,7 @@ class ActualizarEspecialidadView(UpdateView):
 
 class EliminarEspecialidadView(DeleteView):
     model = Especialidad
-    template_name = 'administracion/profesional/eliminarEspecialidad.html'
+    template_name = 'administracion/profesional/especialidad/eliminarEspecialidad.html'
     success_url = reverse_lazy('listadoEspecialidad')
 
     def delete(self, request, *args, **kwargs):
@@ -52,6 +52,38 @@ class EliminarEspecialidadView(DeleteView):
 #VISTA OCUPACION BASADAS EN CLASE
 class ListOcupacion(ListView):
     model = Ocupacion
-    template_name = 'administracion/profesional/listadoOcupaciones.html'  
+    template_name = 'administracion/profesional/ocupacion/listadoOcupacion.html'  
     context_object_name = 'ocupaciones' 
+
+
+class RegistroOcupacionView(CreateView):
+    model = Especialidad
+    form_class = OcupacionForm
+    template_name = 'administracion/profesional/ocupacion/registroOcupacion.html'
+    success_url = reverse_lazy('listadoOcupacion')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Ocupación registrada con éxito.")
+        return super().form_valid(form)
+    
+
+class ActualizarOcupacionView(UpdateView):
+    model = Ocupacion
+    form_class = OcupacionForm
+    template_name = 'administracion/profesional/ocupacion/editarOcupacion.html'
+    success_url = reverse_lazy('listadoOcupacion')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Ocupación editada correctamente.")
+        return super().form_valid(form)
+    
+
+class EliminarOcupacionView(DeleteView):
+    model = Ocupacion
+    template_name = 'administracion/profesional/ocupacion/eliminarOcupacion.html'
+    success_url = reverse_lazy('listadoOcupacion')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Ocupación eliminada correctamente.")
+        return super().delete(request, *args, **kwargs)
  
